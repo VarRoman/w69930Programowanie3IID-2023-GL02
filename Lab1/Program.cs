dbccf0ef4013aba343e3785aae781723916d29ed
VarRoman <bezshchasnyiroman7a76@gmail.com>
2024-11-19 08:55:44 +0100

    Made the Laboratorium #2

---
+++ Lab1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Xml;
+
+namespace Lab1;
+
+public class Program
+{
+    static void FirsTask()
+    {
+        Console.WriteLine("Enter your value");
+        Console.WriteLine((int.Parse(Console.ReadLine()) % 2 == 0) ? "Even" : "Not even");
+    }
+
+    static void SecondTask()
+    {
+        Console.WriteLine("Enter your value");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    static void ThirdTask()
+    {
+        Console.WriteLine("Enter your choice from the menu (1, 2, 4 or 5) or enter word Wyjscie");
+        string n = Console.ReadLine();
+        while (n != "Wyjscie")
+        {
+            switch (n)
+            {
+                case "1":
+                    FirsTask();
+                    break;
+                
+                case "2":
+                    SecondTask();
+                    break;
+                
+                case "4":
+                    Console.WriteLine("Enter the value to get the factorial of it");
+                    int factorial = int.Parse(Console.ReadLine());
+                    Console.WriteLine(FourthTask(factorial));
+                    break;
+                
+                case "5":
+                    FifthTask();
+                    break;
+                
+                default:
+                    Console.WriteLine("You have entered wrong value");
+                    break;
+            }
+            Console.WriteLine("Enter your choice from the menu (1, 2, 4 or 5) or enter word Wyjscie");
+            n = Console.ReadLine();
+        }
+    }
+
+    static int FourthTask(int n)
+    {
+        if (n != 1)
+        {
+            n *= FourthTask(n - 1);
+        }
+
+        return n;
+
+    }
+
+    static void FifthTask()
+    {
+        int countTry = 0;
+        Random random = new Random();
+        int randomNumber = random.Next(0, 51);
+        Console.WriteLine("Try to guess the random number");
+        int guess = int.Parse(Console.ReadLine());
+        while (guess != randomNumber)
+        {
+            if (guess > randomNumber)
+            {
+                Console.WriteLine("Probably close, but you got too far...");
+            }
+            else
+            {
+                Console.WriteLine("Probably close, but that was still too low");
+            }
+            countTry++;
+            guess = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("You have finally guessed right! And the number of guesses was " + countTry + " times!");
+    }
+    
+    
+    static void Main(string[] args)
+    {
+        // FirsTask();
+        // SecondTask();
+        ThirdTask();
+        // Console.WriteLine("Enter the value to get the factorial of it");
+        // int factorial = int.Parse(Console.ReadLine());
+        // Console.WriteLine(FourthTask(factorial));
+        // FifthTask();
+    }
+
+}
---
+++ Lab2/Osoba.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Lab2;
+
+public class Osoba
+{
+    public static int Ilosc = 8;
+    public static string Name { get; set; }
+    public static string Surname { get; set; }
+    public static int Age { get; set; }
+    public static string Pesel { get; set; }
+
+    public Osoba(string name, string surname, int age, string pesel)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+        if (Age < 0)
+        {
+            Age = 0;
+        }
+        Pesel = pesel;
+    }
+
+    public string Introduction()
+    {
+        return $"Nazywam się {Name} {Surname} i mam {Age} lat";
+    }
+
+    public static void Main(string[] args)
+    {
+        //zadanie 1
+        Console.WriteLine("\nZadanie #1");
+        Osoba obj = new Osoba("Roman", "Bezshchasnyi", 19, "12345678888");
+        Console.WriteLine(obj.Introduction());
+        
+        //zadanie 2
+        Console.WriteLine("\nZadanie #2");
+        Licz obj1 = new Licz(30);
+        obj1.Addition(60);
+        obj1.Subtraction(25);
+        Console.WriteLine(obj1.ShowValue());
+        
+        //zadanie 3
+        Console.WriteLine("\nZadanie #3");
+        Licz[] liczba = new Licz[Ilosc];
+        for (int i = 0; i < Ilosc; i++)
+        {
+            liczba[i] = new Licz(i);
+        }
+        Sumator obj2 = new Sumator(liczba);
+        
+        Console.WriteLine("Suma zwracającą sumę liczb z pola Liczby: " + obj2.Suma());
+        Console.WriteLine("SumaPodziel3 zwracającą sumę liczb z tablicy, które są podzielne przez 3: " + obj2.SumaPodziel3());
+        Console.WriteLine("IleElementów() zwracającej liczbę elementów na w tablicy: " + obj2.IleElementów());
+        Console.WriteLine("ShowElements() wypisującą wszystkie elementy tablicy: ");
+        obj2.ShowElements();
+		Console.WriteLine("ShowFromIndexes wypisującą wszystkie elementy tablicy pomiędzy lowindex" +  
+		 " i highindex parametrami: ");
+        obj2.ShowFromIndexes(2, 5);
+    }
+}
+
+public class Licz
+{
+    private int Number { get; set; }
+
+    public Licz(int number)
+    {
+        Number = number;
+    }   
+    public void Addition(int param)
+    {
+        Number += param;
+    }
+
+    public void Subtraction(int param)
+    {
+        Number -= param;
+    }
+
+    public int ShowValue()
+    {
+        return Number;
+    }
+}
+
+public class Sumator
+{
+    private static Licz[] Liczba;
+
+    public Sumator(Licz[] liczba)
+    {   
+        Liczba = liczba;
+        
+    }
+    public int Suma()
+    {
+        int sum = 0;
+        for (int i = 0; i < Liczba.Length; i++)
+        {
+            Licz licz = Liczba[i];
+            sum += licz.ShowValue();
+        }
+        return sum;
+    }
+
+    public int SumaPodziel3()
+    {
+        int suma = 0;
+        foreach (Licz licz in Liczba)
+        {
+            if (suma % 3 == 0)
+            {
+                suma += licz.ShowValue();
+            }
+        }
+        return suma;
+    }
+
+    public int IleElementów()
+    {
+        return Liczba.Length;
+    }
+
+    public void ShowElements()
+    {
+        for (int i = 0; i < Liczba.Length; i++)
+        {
+            Console.WriteLine($"Element {Liczba[i]} ma index {i} w Liczba[] i " +
+                              $"Number ma wartość = {Liczba[i].ShowValue()}");
+            
+        }
+    }
+
+    public void ShowFromIndexes(int lowindex, int highindex)
+    {
+        if (lowindex < 0)
+        {
+            lowindex = 0;
+        }
+
+        if (highindex >= Liczba.Length)
+        {
+            highindex = Liczba.Length - 1;
+        }
+
+        for (int i = lowindex; i <= highindex; i++)
+        {
+            Console.WriteLine($"Element {Liczba[i]} ma index {i} w Liczba[] i " +
+                              $"Number ma wartość = {Liczba[i].ShowValue()}");
+        }
+    }
+}
