90e526ded0251e28d2ceca2ff9537a23b7765539
VarRoman <bezshchasnyiroman7a76@gmail.com>
2025-01-04 19:04:37 +0100

    Created basic objects for project

---
+++ Projekt/BlazorApp1/BasicObjects/Player.cs
@@ -0,0 +1,63 @@
+﻿namespace BlazorApp1;
+
+public class Player
+
+{
+    private string Name { get; set; }
+    private string Surname { get; set; }
+    private int Age { get; set; }
+    private string Gender { get; set; }
+    private string Position { get; set; }
+    private Dictionary<string, double> _playerInfromationStatistics = new Dictionary<string, double>
+    {
+        { "Attacks", 0 },
+        { "Receive", 0 },
+        { "Serves", 0 },
+    };
+    
+    private Dictionary<string, int> _playerInfromationPoints = new Dictionary<string, int>
+    {
+        { "Attacks", 0 },
+        { "Serves", 0 },
+        {"Blocks", 0}
+    };
+    
+    
+    public Player(string name, string surname, int age, string gender, string position)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+        Gender = gender;
+        Position = position;
+    }
+
+    public double GetPlayerInformationStatistics(string key)
+    {
+        if (_playerInfromationStatistics.ContainsKey(key))
+        {
+            return _playerInfromationStatistics[key];
+        }
+        throw new ArgumentException("Invalid key provided.");
+    }
+
+    public int GetPlayerInformationPoints(string key)
+    {
+        if (_playerInfromationPoints.ContainsKey(key))
+        {
+            return _playerInfromationPoints[key];
+        }
+        throw new ArgumentException("Invalid key provided.");
+    }
+
+    public int GetPlayerInformationPointsTotal()
+    {
+        int total = 0;
+        foreach (var key in _playerInfromationPoints.Keys)
+        {
+            total += _playerInfromationPoints[key];
+        }
+        return total;
+    }
+    
+}
---
+++ Projekt/BlazorApp1/BasicObjects/Team.cs
@@ -0,0 +1,36 @@
+﻿namespace BlazorApp1;
+
+public class Team
+{
+    private string TeamName { get; set; }
+    private int RoundScore { get; set; }
+    private int TeamTournamentPlace { get; set; }
+    private string TeamRegistrationPlace { get; set; }
+    private string[] TeamSponsors { get; set; }
+
+    private Dictionary<string, Player> _playersDictionary = new Dictionary<string, Player>();
+
+    public Team(string teamName, string[] teamSponsors, string teamRegistrationPlace, Player setter, Player outside1, 
+        Player outside2, Player opposite, Player libero, Player middle1, Player middle2)
+    {
+        TeamName = teamName;
+        TeamSponsors = teamSponsors;
+        TeamRegistrationPlace = teamRegistrationPlace;
+        _playersDictionary.Add("Setter", setter);
+        _playersDictionary.Add("Outside1", outside1);
+        _playersDictionary.Add("Outside2", outside2);
+        _playersDictionary.Add("Libero", libero);
+        _playersDictionary.Add("Opposite", opposite);
+        _playersDictionary.Add("Middle1", middle1);
+        _playersDictionary.Add("Middle2", middle2);
+    }
+
+    public Player GetPlayer(string playerPosition)
+    {
+        if (_playersDictionary.ContainsKey(playerPosition))
+        {
+            return _playersDictionary[playerPosition];
+        }
+        throw new KeyNotFoundException("Invalid key provided.");
+    }
+}
