86f586981d5c5fe1282fc7bea5a4b9487495b27e
VarRoman <bezshchasnyiroman7a76@gmail.com>
2025-01-11 23:40:19 +0100

    Added Data class and some code for console compilation in Program

--- Projekt1.1/Program.cs
+++ Projekt1.1/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace Projekt1._1;
-using Projekt1._1.BasicObjects;
+using BasicObjects;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Data data = new Data();
+        List<Team> teams = data.Teams;
+        
+        Tournament tournament = new Tournament();
+        
+        foreach (var team in teams)
+        {
+            tournament.AddTeamFirstCycle(team);
+        }
+
+        // Запуск турніру
+        Console.WriteLine("Rozpoczynamy turniej!");
+        tournament.StartTournament();
+
+        // Виведення результатів
+        Console.WriteLine("Wyniki turnieju:");
+        foreach (var winner in tournament.Winners)
+        {
+            Console.WriteLine($"Miejsce {winner.Key}: {winner.Value.TeamName}");
+        }
     }
 }
